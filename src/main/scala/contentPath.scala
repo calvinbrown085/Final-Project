5ac0cb12922ad7b544183ebc13992d3010189a0f
Calvin Brown <calvin.brown@banno.com>
2014-08-04 14:36:31 -0500

    fixed printing wrong title

--- src/main/scala/contentPath.scala
+++ src/main/scala/contentPath.scala
@@ -8,11 +8,10 @@ object ContentSearch {
   def contentSearcher(searchString: String,otherList: List[String],intCount : Int): List[String] = {
     val newList = ListStore.getList()
     var thatList = otherList
-    val newSearch = searchString.map(_.toLower)
     if (intCount >= newList.length - 2) {
       return otherList
     }
-    else if (newList(intCount).toString contains newSearch.toString){
+    else if (newList(intCount).toString contains searchString.toLowerCase){
       thatList = otherList :+ newList(intCount).trim
 
     }
