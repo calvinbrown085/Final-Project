578fefb4ec4e8219574c55e44afcf60e3b6f07d7
Calvin Brown <calvin.brown@banno.com>
2014-08-04 14:00:55 -0500

    fixed search finding the word contained inside a word

--- src/main/scala/articlePath.scala
+++ src/main/scala/articlePath.scala
@@ -7,12 +7,11 @@ def articleSearcher(searchString: String,otherList: List[String],intCount : Int)
     val newList = ListStore.getList()
     val newArt = ListStore.getArticle()
     var thatList = otherList
-    val newSearch = searchString.map(_.toLower)
     val frontSlice = newList(intCount).toString.slice(8,newList(intCount).length)
     if (intCount >= newList.length - 7) {
       return otherList
     }
-    else if (frontSlice.toString.trim == newSearch.toString.trim){
+    else if (frontSlice.toString.trim == searchString.toLowerCase.trim){
       thatList = otherList :+ newArt.trim
 
 
