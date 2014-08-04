9eef02b96da36148f2b85d652fde3332634df45f
Calvin Brown <calvin.brown@banno.com>
2014-08-04 14:58:20 -0500

    prints entire article when searching title

--- src/main/scala/titlePath.scala
+++ src/main/scala/titlePath.scala
@@ -12,7 +12,13 @@ object TitleSearch {
     }
     else if (newList(intCount).toString.toLowerCase contains " "+searchString.toLowerCase){
       thatList = otherList :+ newList(intCount).trim
-
+      println("Id: "+newList(intCount - 1))
+      println("Title: "+newList(intCount))
+      println("Author: "+newList(intCount + 1))
+      println("Published: "+newList(intCount + 2))
+      println("Updated: "+newList(intCount + 3))
+      println("Content: "+newList(intCount + 4))
+      println("______________________________________________________________________")
     }
     titleSearcher(searchString,thatList,intCount + 8)
 
