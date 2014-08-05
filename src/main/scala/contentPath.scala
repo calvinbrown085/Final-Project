61d248c13b7bc627647330d2e92d7e2b90442dbe
Calvin Brown <calvin.brown@banno.com>
2014-08-05 09:02:58 -0500

    fixed up content not finding search querey

--- src/main/scala/contentPath.scala
+++ src/main/scala/contentPath.scala
@@ -11,8 +11,16 @@ object ContentSearch {
     if (intCount >= newList.length - 2) {
       return otherList
     }
-    else if (newList(intCount).toString contains searchString.toLowerCase){
+    else if (newList(intCount).toString.toLowerCase contains " "+searchString.toLowerCase){
       thatList = otherList :+ newList(intCount).trim
+      println("These are the articles I found with your search criteria on the content")
+      println(newList(intCount - 5))
+      println(newList(intCount - 4))
+      println(newList(intCount - 3))
+      println(newList(intCount -2))
+      println(newList(intCount - 1))
+      println(newList(intCount))
+      println("______________________________________________________________________________")
 
     }
     contentSearcher(searchString,thatList,intCount + 8)
