0469082d39efb62a27914d43ff192336be6a8bba
Calvin Brown <calvin.brown@banno.com>
2014-08-07 11:20:26 -0500

    renamed file

--- src/main/scala/contentSearcher.scala
+++ src/main/scala/contentSearcher.scala
@@ -5,14 +5,14 @@ package com.banno.finalProj
 object ContentSearch {
 
 
-  def contentSearcher(searchString: String,otherList: List[String],searchCount : Int): List[String] = {
+  def contentSearcher(searchString: String,returnList: List[String],searchCount : Int): List[String] = {
     val articleList = ListStore.getList()
     var totalContentList = articleList
     if (searchCount >= articleList.length - 2) {
       return totalContentList
     }
     else if (articleList(searchCount).toString.toLowerCase contains " "+searchString.toLowerCase){
-      totalContentList = otherList :+ articleList(searchCount).trim
+      totalContentList = returnList :+ articleList(searchCount).trim
       println("These are the articles I found with your search criteria"+" "+searchString+" "+"on the content")
       println(articleList(searchCount - 5))
       println(articleList(searchCount - 4))
